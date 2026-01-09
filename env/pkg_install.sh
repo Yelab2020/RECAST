c64b19162dbe5f36d8fb67ce328d1f24f7bb2fbf
Alex <1004923690@qq.com>
2026-01-09 15:17:44 +0800

    Add files via upload

---
+++ env/pkg_install.sh
@@ -0,0 +1,13 @@
+#conda create -n M3Spade python==3.8.20
+#conda activate M3Spade
+conda install pytorch==2.2.2 torchvision==0.17.2 torchaudio==2.2.2 pytorch-cuda=11.8 -c pytorch -c nvidia -y
+conda install pyg -c pyg -y
+conda install -c dglteam/label/th22_cu118 dgl -y
+conda install pydantic -y
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple rdkit
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple dgllife
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple scipy==1.10.1
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple numpy==1.24.3
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple scanpy==1.9.8
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple imbalanced-learn==0.12.4
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple esda==2.5.1
